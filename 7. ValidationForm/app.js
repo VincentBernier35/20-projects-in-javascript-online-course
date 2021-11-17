adf3374b65da9c98343cb56430c35d82f114275e
Vincent Bernier <72205969+VincentBernier35@users.noreply.github.com>
2021-11-17 17:34:08 +0100

    I try to do it again, to practice and to remember it !

--- 7. ValidationForm/app.js
+++ 7. ValidationForm/app.js
@@ -9,136 +9,245 @@ const allLigne = document.querySelectorAll('.ligne div');
 
 inpUtilisateur.addEventListener('input', (e) => {
 
-    if(e.target.value.length >= 3) {
+    if (e.target.value.length >= 3) {
         allImg[0].style.display = "inline";
         allImg[0].src = "ressources/check.svg";
         allSpan[0].style.display = "none";
-    }   
+    }
     else {
         allImg[0].style.display = "inline";
         allImg[0].src = "ressources/error.svg";
         allSpan[0].style.display = "inline";
     }
-
 })
 
 inpMail.addEventListener('input', (e) => {
 
     const regexEmail = /\S+@\S+\.\S+/;
-    
-    if(e.target.value.search(regexEmail) === 0){
 
+    if(e.target.value.search(regexEmail) === 0) {
         allImg[1].style.display = "inline";
         allImg[1].src = "ressources/check.svg";
         allSpan[1].style.display = "none";
-
-    } else if(e.target.value.search(regexEmail) === -1) {
-
+    }
+    else if (e.target.value.search(regexEmail) === -1) {
         allImg[1].style.display = "inline";
         allImg[1].src = "ressources/error.svg";
         allSpan[1].style.display = "inline";
-
     }
-
 })
 
-// Validation création du MDP
+// validation création du mdp
 
 let valeurInp;
 const specialCar = /[^a-zA-Z0-9]/;
 const alphabet = /[a-z]/i;
 const chiffres = /[0-9]/;
 
-let objValidation = {
-    symbole : 0,
-    lettre : 0,
-    chiffre : 0
+let oblValidation = {
+    symbole: 0,
+    lettre: 0,
+    chiffre: 0
 }
 
-inpMdp.addEventListener('input', (e) => {
-
-    valeurInp = e.target.value;
-
-    if(valeurInp.search(specialCar) !== -1){
-        objValidation.symbole = 1;
-    }
-    if(valeurInp.search(alphabet) !== -1){
-        objValidation.lettre = 1;
-    }
-    if(valeurInp.search(chiffres) !== -1){
-        objValidation.chiffre = 1;
-    }
-
-
-
-    if(e.inputType = 'deleteContentBackward'){
-        if(valeurInp.search(specialCar) === -1){
-            objValidation.symbole = 0;
-        }
-        if(valeurInp.search(alphabet) === -1){
-            objValidation.lettre = 0;
-        }
-        if(valeurInp.search(chiffres) === -1){
-            objValidation.chiffre = 0;
-        }
-    } 
-
-    let testAll = 0;
-    for(const property in objValidation){
-        if(objValidation[property] > 0){
-            testAll++;
-        }
-    }
-    if(testAll < 3){
-        allSpan[2].style.display = "inline";
-        allImg[2].style.display = "inline";
-        allImg[2].src = "ressources/error.svg";
-    } else {
-        allSpan[2].style.display = "none";
-        allImg[2].src = "ressources/check.svg";
-    }
-
-
-    // force mdp
-    if(valeurInp.length <= 6 && valeurInp.length > 0){
-        allLigne[0].style.display = 'block';
-        allLigne[1].style.display = 'none';
-        allLigne[2].style.display = 'none';
-    }
-    else if (valeurInp.length > 6 && valeurInp.length <= 9) {
-        allLigne[0].style.display = 'block';
-        allLigne[1].style.display = 'block';
-        allLigne[2].style.display = 'none';
-    }
-    else if (valeurInp.length > 9) {
-        allLigne[0].style.display = 'block';
-        allLigne[1].style.display = 'block';
-        allLigne[2].style.display = 'block';
-    }
-    else if (valeurInp.length === 0) {
-        allLigne[0].style.display = 'none';
-        allLigne[1].style.display = 'none';
-        allLigne[2].style.display = 'none';
-    }
+inpMdp = document.addEventListener('input', (e) => {
 
 
 })
 
 
-// confirmation
 
-inpConfirme.addEventListener('input', (e) => {
 
-    if(e.target.value.length === 0){
-        allImg[3].style.display = "inline";
-        allImg[3].src = "ressources/error.svg";
-    }
-    else if(e.target.value === valeurInp){
-        allImg[3].style.display = "inline";
-        allImg[3].src = "ressources/check.svg";
-    } else {
-        allImg[3].style.display = "inline";
-        allImg[3].src = "ressources/error.svg";
-    }
 
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// inpUtilisateur.addEventListener('input', (e) => {
+
+//     if(e.target.value.length >= 3) {
+//         allImg[0].style.display = "inline";
+//         allImg[0].src = "ressources/check.svg";
+//         allSpan[0].style.display = "none";
+//     }   
+//     else {
+//         allImg[0].style.display = "inline";
+//         allImg[0].src = "ressources/error.svg";
+//         allSpan[0].style.display = "inline";
+//     }
+
+// })
+
+// inpMail.addEventListener('input', (e) => {
+
+//     const regexEmail = /\S+@\S+\.\S+/;
+    
+//     if(e.target.value.search(regexEmail) === 0){
+
+//         allImg[1].style.display = "inline";
+//         allImg[1].src = "ressources/check.svg";
+//         allSpan[1].style.display = "none";
+
+//     } else if(e.target.value.search(regexEmail) === -1) {
+
+//         allImg[1].style.display = "inline";
+//         allImg[1].src = "ressources/error.svg";
+//         allSpan[1].style.display = "inline";
+
+//     }
+
+// })
+
+// // Validation création du MDP
+
+// let valeurInp;
+// const specialCar = /[^a-zA-Z0-9]/;
+// const alphabet = /[a-z]/i;
+// const chiffres = /[0-9]/;
+
+// let objValidation = {
+//     symbole : 0,
+//     lettre : 0,
+//     chiffre : 0
+// }
+
+// inpMdp.addEventListener('input', (e) => {
+
+//     valeurInp = e.target.value;
+
+//     if(valeurInp.search(specialCar) !== -1){
+//         objValidation.symbole = 1;
+//     }
+//     if(valeurInp.search(alphabet) !== -1){
+//         objValidation.lettre = 1;
+//     }
+//     if(valeurInp.search(chiffres) !== -1){
+//         objValidation.chiffre = 1;
+//     }
+
+
+
+//     if(e.inputType = 'deleteContentBackward'){
+//         if(valeurInp.search(specialCar) === -1){
+//             objValidation.symbole = 0;
+//         }
+//         if(valeurInp.search(alphabet) === -1){
+//             objValidation.lettre = 0;
+//         }
+//         if(valeurInp.search(chiffres) === -1){
+//             objValidation.chiffre = 0;
+//         }
+//     } 
+
+//     let testAll = 0;
+//     for(const property in objValidation){
+//         if(objValidation[property] > 0){
+//             testAll++;
+//         }
+//     }
+//     if(testAll < 3){
+//         allSpan[2].style.display = "inline";
+//         allImg[2].style.display = "inline";
+//         allImg[2].src = "ressources/error.svg";
+//     } else {
+//         allSpan[2].style.display = "none";
+//         allImg[2].src = "ressources/check.svg";
+//     }
+
+
+//     // force mdp
+//     if(valeurInp.length <= 6 && valeurInp.length > 0){
+//         allLigne[0].style.display = 'block';
+//         allLigne[1].style.display = 'none';
+//         allLigne[2].style.display = 'none';
+//     }
+//     else if (valeurInp.length > 6 && valeurInp.length <= 9) {
+//         allLigne[0].style.display = 'block';
+//         allLigne[1].style.display = 'block';
+//         allLigne[2].style.display = 'none';
+//     }
+//     else if (valeurInp.length > 9) {
+//         allLigne[0].style.display = 'block';
+//         allLigne[1].style.display = 'block';
+//         allLigne[2].style.display = 'block';
+//     }
+//     else if (valeurInp.length === 0) {
+//         allLigne[0].style.display = 'none';
+//         allLigne[1].style.display = 'none';
+//         allLigne[2].style.display = 'none';
+//     }
+
+
+// })
+
+
+// // confirmation
+
+// inpConfirme.addEventListener('input', (e) => {
+
+//     if(e.target.value.length === 0){
+//         allImg[3].style.display = "inline";
+//         allImg[3].src = "ressources/error.svg";
+//     }
+//     else if(e.target.value === valeurInp){
+//         allImg[3].style.display = "inline";
+//         allImg[3].src = "ressources/check.svg";
+//     } else {
+//         allImg[3].style.display = "inline";
+//         allImg[3].src = "ressources/error.svg";
+//     }
+
+// })
